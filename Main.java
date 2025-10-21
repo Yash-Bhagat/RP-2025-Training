ac38f32fdca7b35ea120636b1c538858a88b5aaa
Yash-Bhagat <Byash6420@gmail.com>
2025-10-21 19:52:55 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+// Online Java - IDE, Code Editor, Compiler
+
+// Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+
+// Write your Java code here
+
+public class Main {
+  public static void main(String[] args) {
+    
+    Book book1 = new Book("Intro to Git", 499.99); 
+    Book book2 = new Book("Gautum is selling", 2.99);
+  }
+}
