af599008d3185a821aad554ab6f16354a0c5e284
Yash-Bhagat <Byash6420@gmail.com>
2025-10-21 20:16:54 -0400

    Create Textbook

---
+++ TextBook.java
@@ -0,0 +1,10 @@
+public TextBook extends Book{
+    
+    public TextBook(String title, double price, int editionNumber){
+        
+        super(title, price); 
+        this.editionNumber = editionNumber; 
+    }
+ 
+}
+}
